9400b1626ba3ba9be3901d9434a027fc24893840
Nick Driscoll <nick.a.mavros@gmail.com>
2019-02-03 20:29:46 -0500

    We did it

--- Assignment1/js/main.js
+++ Assignment1/js/main.js
@@ -17,7 +17,7 @@ function sphere_prim(radius, widthSegments, heightSegments, c) {
 }
 
 function cylinder_prim(radius, height, radialSegments, heightSegments, c) {
-	var geo = new THREE.CylinderGeometry(radius, radius, radialSegments, heightSegments);
+	var geo = new THREE.CylinderGeometry(radius, radius, height, radialSegments, heightSegments);
 	var mat = new THREE.MeshBasicMaterial( {color: c, wireframe: true} );
 	return new THREE.Mesh(geo, mat);
 }
@@ -37,6 +37,8 @@ document.body.appendChild( renderer.domElement );
 
 //Create our geometries
 var plane = plane_prim(15, 15, 0xFFFFFF);
+plane.rotation.x += 1.5;
+plane.position.y -= 3;
 scene.add( plane );
 
 var left_foot = cone_prim(1, 2, 8, 1, 0xFF0000);
@@ -49,12 +51,28 @@ right_foot.position.x += 3;
 right_foot.position.y -= 3;
 scene.add( right_foot );
 
+var left_leg = cylinder_prim(1, 4, 8, 1, 0x0000FF);
+left_leg.position.x -= 3;
+scene.add(left_leg);
+
+var right_leg = cylinder_prim(1, 4, 8, 1, 0x0000FF);
+right_leg.position.x += 3;
+scene.add(right_leg);
+
+var body = cube_prim(4, 0x00FF00);
+body.position.y += 4;
+scene.add(body);
+
+var head = sphere_prim(3, 8, 6, 0xFFFFFF);
+head.position.y += 9;
+scene.add(head);
+
 
 camera.position.z = 15;
+camera.position.y += 3;
 
 function animate() {
 	requestAnimationFrame( animate );
-	console.log(left_foot.rotation.x);
 	renderer.render( scene, camera );
 }
 animate();

f4ebe04d782494047a9b9b1b58e49388bce611b3
Nick Driscoll <nick.a.mavros@gmail.com>
2019-02-15 16:28:15 -0500

    Started assignment 2

---
+++ Assignment2/js/main.js
@@ -0,0 +1,121 @@
+function plane_prim(width, height, c) {
+	var geo = new THREE.PlaneGeometry(width, height);
+	var mat = new THREE.MeshBasicMaterial( {color: c, wireframe: false} );
+	return new THREE.Mesh(geo, mat);
+}
+
+function cube_prim(length, c) {
+	var geo = new THREE.BoxGeometry(length, length, length);
+	var mat = new THREE.MeshBasicMaterial( {color: c, wireframe: false } );
+	return new THREE.Mesh(geo, mat);
+}
+
+function sphere_prim(radius, widthSegments, heightSegments, c) {
+	var geo = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+	var mat = new THREE.MeshBasicMaterial( {color: c, wireframe: false} );
+	return new THREE.Mesh(geo, mat);
+}
+
+function cylinder_prim(radius, height, radialSegments, heightSegments, c) {
+	var geo = new THREE.CylinderGeometry(radius, radius, height, radialSegments, heightSegments);
+	var mat = new THREE.MeshBasicMaterial( {color: c, wireframe: false} );
+	return new THREE.Mesh(geo, mat);
+}
+
+function cone_prim(radius, height, radialSegments, heightSegments, c) {
+	var geo = new THREE.ConeGeometry(radius, height, radialSegments, heightSegments);
+	var mat = new THREE.MeshBasicMaterial( {color: c, wireframe: false} );
+	return new THREE.Mesh(geo, mat);
+}
+
+var views = [
+	{
+		left: 0,
+		top: 0,
+		width: 0.5,
+		height: 1.0,
+		background: new THREE.Color( 0.5, 0.5, 0.7 ),
+		eye: [ 0, 0, 0 ],
+		up: [ 0, 1, 0 ],
+		fov: 75,
+		updateCamera: function ( camera, scene, mouseX ) {
+		  camera.position.x += mouseX * 0.05;
+		  camera.position.x = Math.max( Math.min( camera.position.x, 2000 ), - 2000 );
+		  camera.lookAt( scene.position );
+		}
+	},
+	{
+		left: 0.5,
+		top: 0.5,
+		width: 0.5,
+		height: 0.5,
+		background: new THREE.Color( 0.7, 0.5, 0.5 ),
+		eye: [ 0, 1800, 0 ],
+		up: [ 0, 0, 1 ],
+		fov: 45,
+		updateCamera: function ( camera, scene, mouseX ) {
+		  camera.position.x -= mouseX * 0.05;
+		  camera.position.x = Math.max( Math.min( camera.position.x, 2000 ), - 2000 );
+		  camera.lookAt( camera.position.clone().setY( 0 ) );
+		}
+	}
+];
+
+for (var i = 0; i < views.length; i++) {
+	var view = views[i];
+	var camera = new THREE.PerspectiveCamera(view.fov, window.innerWidth / window.innerHeight, 1, 10000);
+	camera.position.fromArray( view.eye );
+	camera.up.fromArray( view.up );
+	view.camera = camera;
+}
+
+var scene = new THREE.Scene();
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+//Create our geometries
+var plane = plane_prim(15, 15, 0xFFFFFF);
+plane.rotation.x += 1.5;
+plane.position.y -= 3;
+scene.add( plane );
+
+var left_foot = cone_prim(1, 2, 8, 1, 0xFF0000);
+left_foot.position.x -= 3;
+left_foot.position.y -= 3;
+scene.add( left_foot );
+
+var right_foot = cone_prim(1, 2, 8, 1, 0xFF0000);
+right_foot.position.x += 3;
+right_foot.position.y -= 3;
+scene.add( right_foot );
+
+var left_leg = cylinder_prim(1, 4, 8, 1, 0x0000FF);
+left_leg.position.x -= 3;
+scene.add(left_leg);
+
+var right_leg = cylinder_prim(1, 4, 8, 1, 0x0000FF);
+right_leg.position.x += 3;
+scene.add(right_leg);
+
+var body = cube_prim(4, 0x00FF00);
+body.position.y += 4;
+scene.add(body);
+
+var head = sphere_prim(3, 8, 6, 0xFFFFFF);
+head.position.y += 9;
+scene.add(head);
+
+
+views[0].camera.position.z = 15;
+views[0].camera.position.y += 3;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	
+
+	renderer.render( scene, views[0].camera );
+}
+animate();
